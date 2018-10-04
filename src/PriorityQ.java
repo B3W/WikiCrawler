68fbfb15c1d652e63673d8ccf333d0dc1f90a904
Weston Berg <westieberg@gmail.com>
2018-10-04 17:40:20 -0500

    Initial Commit. Start of Priority Queue for WikiCrawler.

---
+++ src/PriorityQ.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class PriorityQ {
+
+	/**
+	 * Constructs empty priority queue
+	 */
+	public PriorityQ() {
+		
+	}
+	
+	public void add(String s, int p) {
+		
+	}
+	
+	public String returnMax() {
+		return "";
+	}
+	
+	public String extractMax() {
+		return "";
+	}
+	
+	public void remove(int i) {
+		
+	}
+	
+	public void decrementPriority(int i, int k) {
+		
+	}
+	
+	public ArrayList<Integer> priorityArray() {
+		return null;
+	}
+	
+	
+}
